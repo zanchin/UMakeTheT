8e89106b77c7075c86eef7157f5c4234f15bbf47
Ilya Braude <ilya@cloudmine.me>
2012-07-11 10:36:35 -0400

    fancy t-shirt style selector

--- base.js
+++ base.js
@@ -5,18 +5,83 @@
     });
 
     $("div #size").on('click', function() {
-      DT.go('type');
+      DT.go('style');
     });
 
     $("div #type").on('click', function() {
       DT.go('home');
     });
 
-    $("#size").ddslick();
-    $("#type").ddslick();
+    var sizeData = [
+      {
+        text: "Small",
+        value: "S",
+        selected: false
+//        description: "Small",
+//        imageSrc: "http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png"
+      },
+      {
+        text: "Medium",
+        value: "Medium",
+        selected: false
+      },
+      {
+        text: "Large",
+        value: "Large",
+        selected: false
+      },
+      {
+        text: "X-Large",
+        value: "XLarge",
+        selected: false
+      }
+    ];
+
+    $("#size").ddslick({
+      data: sizeData,
+      selectText: "Select the T size",
+      onSelected: function(data){
+        //console.log(data.selectedData.text);
+        DT.setCurrent({size: data.selectedData.text});
+      }
+    });
+
+    $("#style").ddslick({
+      data: [
+        {
+          text: "T-Shirt", value: "tshirt", selected: false,
+          imageSrc: "images/tshirt.png"
+        },
+        {
+          text: "Polo", value: "polo", imageSrc: "images/polo.png"
+        },
+        {
+          text: "Henley", value: "henley", imageSrc: "images/henley.png"
+        }
+      ],
+      selectText: "Select the T type",
+      onSelected: function(data){
+        DT.setCurrent({style: data.selectedData.text});
+      }
+    });
   });
 
   var DT = {
+    current: {
+      size: "Medium",
+      style: ""
+    },
+    setCurrent: function(data){
+      // getto merge
+      for(d in data){
+        if(data.hasOwnProperty(d)){
+          DT.current[d] = data[d];
+        }
+      }
+
+      // console.log(JSON.stringify(DT.current));
+    },
+
     navigate: function(url){
       document.location.href = url;
     },
@@ -27,25 +92,15 @@
     go: function(loc){
       var ids = {
         'size': 'sizecontent',
-        'type': 'typecontent',
+        'style': 'stylecontent',
         'cc': 'cccontent',
         'home': 'homecontent'
-      }
+      };
 
       var contentid = ids[loc] || ids.home;
       DT.showcontent(contentid);
-
-      // var urls = {
-      //   'size': 'size.html',
-      //   'type': 'type.html',
-      //   'cc': 'cc.html',
-      //   'home': 'index.html'
-      // }
-
-      // var url = urls[loc] || urls.home;
-      // DT.navigate(url);
     }
-  }
+  };
 
   window.DT = DT;
 })();
